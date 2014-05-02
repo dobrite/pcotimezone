c46ec47d80b372165f768a010619eeb0327dad08
David Obrite <david@ministrycentered.com>
2014-05-02 13:56:30 -0700

    added CORS headers

--- web.js
+++ web.js
@@ -8,7 +8,13 @@ tzwhere.init();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/', function(req, res, next) {
   var lat = req.query.lat;
   var lng = req.query.lng;
   var tz  = tzwhere.tzNameAt(lat, lng);
@@ -16,6 +22,7 @@ app.get('/', function(req, res) {
 });
 
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
