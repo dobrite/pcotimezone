271726f1e5ce11c07241fc29883990c16c9ce4c7
Dave O <davidobrite@gmail.com>
2014-05-02 20:53:39 -0700

    first stab at it

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var tzwhere = require("tzwhere");
+var express   = require("express");
+var logfmt    = require("logfmt");
+var tzwhere   = require("tzwhere");
+var BigNumber = require("bignumber.js");
 var app = express();
 
 tzwhere.init();
@@ -17,8 +18,28 @@ app.all('*', function(req, res, next) {
 app.get('/', function(req, res, next) {
   var lat = req.query.lat;
   var lng = req.query.lng;
-  var tz  = tzwhere.tzNameAt(lat, lng);
-  res.json({tz: tz || 'etc/utc'});
+
+  if (!(lat && lng)) {
+    res.statusCode = 422;
+    return res.json({error: 'must pass in lat and lng as query args'});
+  }
+
+  var bLat = BigNumber(lat);
+
+  if (bLat.gte(BigNumber(85.0)) || bLat.lte(BigNumber(-85.05115))) {
+    res.statusCode = 422;
+    return res.json({error: 'lat must be between -85.05115 and 85.0'});
+  }
+
+  var bLng = BigNumber(lng);
+
+  if (bLng.gte(BigNumber(180.0)) || bLng.lte(BigNumber(-180.0))) {
+    res.statusCode = 422;
+    return res.json({error: 'lng must be between -180.0 and 180.0'});
+  }
+
+  var tz  = tzwhere.tzNameAt(bLat.toFixed(), bLng.toFixed());
+  return res.json({tz: tz || 'etc/utc'});
 });
 
 var port = Number(process.env.PORT || 5000);
